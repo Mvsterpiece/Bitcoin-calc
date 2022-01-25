422e2938de6e8a1b1ad0eea1c7ab19bb9ec13f63
opilane <opilane@HP-CZC0359NN8>
2022-01-25 13:27:42 +0200

    Add project files.

---
+++ Bitcoin calc/Program.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+
+namespace Bitcoin_calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BitcoinRate bitcoin = GetRates();
+            Console.WriteLine($"Current rate in {bitcoin.bpi.USD.code}:{bitcoin.bpi.USD.rate_float}");
+            Console.WriteLine($"Current rate in {bitcoin.bpi.EUR.code}:{bitcoin.bpi.EUR.rate_float}");
+            Console.WriteLine($"Current rate in {bitcoin.bpi.GBP.code}:{bitcoin.bpi.GBP.rate_float}");
+
+        }
+
+        public static BitcoinRate GetRates()
+        {
+            string url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            BitcoinRate bitcoin;
+
+            using (var responseReader = new StreamReader(webStream));
+            {
+                var response = responseReader.ReadToEnd();
+                bitcoin = JsonConvert.DeserializeObject<BitcoinRate>(response);
+
+            }
+            return bitcoin;
+        }
+
+
+    }
+}
